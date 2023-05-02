21cf24c2adf0c0595da5dcc444479b1cc824d601
greenbel <obisann@gmail.com>
2023-05-02 18:35:30 +0000

    0x02-T0: added documentation on process.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,8 +19,8 @@ def getPrimeDivisor(n: int) -> int:
         if remainder != 0:
             divisor += 1
         if divisor > limit:
-            # no number can be divided by a value more than half its own
-            return n
+            # no number can be divided by a value more than half its own...
+            return n  # ...except that value is itself
 
     # remainder equals 0
     return divisor
@@ -33,7 +33,37 @@ def minOperations(n: int) -> int:
     In a text file, there is a single character H. Your text editor can
     ...execute only two operations in this file: Copy All and Paste.
 
-    Implemented using recursion.
+    Process:
+        Given a target number of characters, 18, and
+        ...starting with one character. I figured the fastest way to
+        ...get to 18 was to copy 9 characters (18 / 2) and paste them
+        ...once (a total of 2 operations). But then, how do you get to
+        ...9 characters, fastest? 9 is not divisible by 2 but 3 is.
+        So the fastest way to get to 9 characters is to
+        ...copy 3 characters (9 / 3) and paste them two
+        ...times (a total of 3 operations).
+        Next, how do you get to three characters? 3 is
+        ...not divisible by 2, but 3 is. So the fastest way to
+        ...get to 3 characters is to copy 1 character (3 / 3) and
+        paste them two times (a total of 3 operations).
+
+        This produced a pattern that led to my implementation,
+        ...using recursion. A key part of this observed pattern is that
+        ...the divisor of the target number equals the number of operations to
+        ...be performed to achieve the end goal of reaching the
+        ...target number at each stage of the breakdown (18 to 9 to 3).
+
+        I needed to figure out the number of characters to copy and paste,
+        ...in the minimum amount of times to reach a target number.
+        The solution was to divide the target number by
+        ...its smallest prime factor. The quotient is guaranteed to be
+        ...greater than or equal to the divisor, and if you add
+        ...this quotient the number of times of the divisor,
+        ...you arrive at the target number in the fewest additions.
+
+        I implemented a helper function to produce the
+        ...magic prime number. And once I figured out the
+        ...base case and flow of the recursion, I was all set.
     '''
     if n < 2:
         # This targets the first/initial value of n
