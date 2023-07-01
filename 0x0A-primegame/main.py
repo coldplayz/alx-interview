f34f11068e58749c64f3bfce977b59f9bb14d396
greenbel <obisann@gmail.com>
2023-07-01 19:45:32 +0000

    0x0A: try a more efficient implementation

--- 0x0A-primegame/main.py
+++ 0x0A-primegame/main.py
@@ -4,3 +4,9 @@ isWinner = __import__('0-prime_game').isWinner
 
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+
+nums = [0] * 10000
+for i in range(10000):
+    nums[i] = i
+
+print("Winner: {}".format(isWinner(10000, nums)))
